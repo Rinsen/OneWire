9b44e30bf93fc8b75961d5947961f293f71460b3
Rinsen <fredrikh@outlook.com>
2015-06-29 23:43:22 +0200

    Added nuget nuspec

--- src/Rinsen.IoT.OneWire/Properties/AssemblyInfo.cs
+++ src/Rinsen.IoT.OneWire/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Rinsen.IoT.OneWire")]
-[assembly: AssemblyDescription("One wire library for Windows IoT via DS2482-100")]
+[assembly: AssemblyDescription("One Wire library for Windows IoT via DS2482-100")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Fredrik Rinsén")]
 [assembly: AssemblyProduct("Rinsen.IoT.OneWire")]
@@ -23,6 +23,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
 [assembly: ComVisible(false)]
