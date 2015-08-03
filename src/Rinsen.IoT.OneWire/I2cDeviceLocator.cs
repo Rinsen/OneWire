7dfb8ab113220264893856a4678eb138fbc463ec
Rinsen <fredrikh@outlook.com>
2015-08-03 16:47:15 +0200

    Added i2c controller name

--- src/Rinsen.IoT.OneWire/I2cDeviceLocator.cs
+++ src/Rinsen.IoT.OneWire/I2cDeviceLocator.cs
@@ -7,9 +7,12 @@ namespace Rinsen.IoT.OneWire
 {
     internal class I2cDeviceLocator
     {
+        //private const string I2C_CONTROLLER_NAME = "I2C5";        // For Minnowboard Max, use I2C5
+        private const string I2C_CONTROLLER_NAME = "I2C1";        // For Raspberry Pi 2, use I2C1
+
         public async Task<I2cDevice> GetI2cDevice(byte address)
         {
-            string aqs = I2cDevice.GetDeviceSelector();                     /* Get a selector string that will return all I2C controllers on the system */
+            string aqs = I2cDevice.GetDeviceSelector(I2C_CONTROLLER_NAME);                     /* Get a selector string that will return all I2C controllers on the system */
             var dis = await DeviceInformation.FindAllAsync(aqs);            /* Find the I2C bus controller device with our selector string           */
             if (dis.Count == 0)
             {
